eda8905f4f077547afc7b01b6ef33594b272df52
ShleeRE <konradbralewski1111@gmail.com>
2025-01-24 20:52:03 +0100

    prioriytet dla taskow

--- src/api/tasks.ts
+++ src/api/tasks.ts
@@ -1,49 +1,84 @@
-import { ITask, ITeam } from "../types.ts";
+import { ITask } from "../types.ts";
 import { privateApi } from "./privateApi.ts";
 
-  export const createTask= async ({ title, project, description, progress, story_points, created_by }: ITask) => {  
+export const createTask = async ({ 
+    title, 
+    project, 
+    description, 
+    progress, 
+    story_points, 
+    created_by, 
+    priority 
+}: ITask) => {  
     try {
-      const resp = await privateApi(`/api/tasks/`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ title, project, description, progress, story_points, created_by })
-      });
-  
-      return resp
+        const resp = await privateApi(`/api/tasks/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ 
+                title, 
+                project, 
+                description, 
+                progress, 
+                story_points, 
+                created_by, 
+                priority 
+            })
+        });
+        return resp;
     } catch (error) {
-        throw new Error(error)
+        throw new Error(error);
     }
-  };
+};
 
-  export const updateTask = async ({ title, project, description, progress, story_points, id }: ITask) => {  
+export const updateTask = async ({ 
+    title, 
+    project, 
+    description, 
+    progress, 
+    story_points, 
+    id, 
+    priority, 
+    approved_by_tester 
+}: ITask) => {  
     try {
-      const resp = await privateApi(`/api/tasks/${id}/`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({title, project, description, progress, story_points })
-      });
-  
-      return resp
-    } catch (error) {
-        throw new Error(error)
-    }
-  };
+        const bodyData: any = { 
+            title, 
+            project, 
+            description, 
+            progress, 
+            story_points, 
+            priority 
+        };
+        
+        if (typeof approved_by_tester !== 'undefined') {
+            bodyData.approved_by_tester = approved_by_tester;
+        }
 
-  export const deleteTask = async (id: number) => {  
-    try {
-      const resp = await privateApi(`/api/tasks/${id}/`, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      return resp
+        const resp = await privateApi(`/api/tasks/${id}/`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(bodyData)
+        });
+        return resp;
     } catch (error) {
-        throw new Error(error)
+        throw new Error(error);
     }
-  };
+};
+
+export const deleteTask = async (id: number) => {  
+    try {
+        const resp = await privateApi(`/api/tasks/${id}/`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        return resp;
+    } catch (error) {
+        throw new Error(error);
+    }
+};

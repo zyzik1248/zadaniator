880e5648271c9fc84b4d421714362fe198ca65fb
Bartlomiej <bartlomiej.rosinski99@gmail.com>
2025-01-25 16:51:50 +0100

    Przycisk LOGOUT

--- src/component/nav/Navs.tsx
+++ src/component/nav/Navs.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import "./Navs.scss"
 import teams from "./../../assets/Teams.png"
 import tasks from "./../../assets/Task.png"
-import settings from "./../../assets/Setting 2.png"
 import user from "./../../assets/User.png"
 import teamsH from "./../../assets/Teams-h.png"
 import taskH from "./../../assets/Task-h.png"
@@ -50,8 +49,7 @@ const Navs:React.FC<IProps> = ({open, setOpen}) => {
                 <Nav onClick={()=>setOpen(!open)} className={`menu ${open ? "active" : ""}`} icon={menu} iconH={menuH} isLink={false}/>
             </div>
             <div className="navs-settings">
-                <Nav to={"settings"} icon={settings}/>
-                <Nav to={"profile"} icon={user} className="user"/>
+                <Nav to={"/Login"} icon={user} className="user"/>
             </div>
         </nav>
     )
